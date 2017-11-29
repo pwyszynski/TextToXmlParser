60a8cb71898891058521f3d7d365286399824438
Przemyslaw Wyszynski <wyszynski.p@windowslive.com>
2017-11-29 16:04:05 +0100

    Added TextToXML parser controller to WebApi

---
+++ Parser.API/Controllers/XmlParserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Parser.Utils;
+
+namespace Parser.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class XmlParserController : Controller
+    {
+        [HttpPost("[action]")]
+        public string ParseSentence([FromBody] string inputText)
+        {
+            return XMLParser.ParseText(inputText);
+        }
+    }
+}
