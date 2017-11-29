61b48e3e7a5e72fd14434f0f50af21eea00b18b0
Wyszynski <przemyslawx.wyszynski@intel.com>
2017-11-29 12:18:25 +0100

    Added XML parser logic

---
+++ Services/XMLParserService/XMLParser.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+using ConsoleParsing.Models;
+
+namespace ConsoleParsing.Utils
+{
+    public static class XMLParser
+    {
+        public static string ParseText(string receivedText)
+        {
+            var t = XMLParser.SplitToText(receivedText);
+
+            return XMLParser.ParseToXml(t);
+        }
+
+        private static string ParseToXml(Text t)
+        {
+            var serializer = new XmlSerializer(typeof(Text));
+            using (var sw = new StringWriter())
+            {
+                serializer.Serialize(sw, t);
+
+                return sw.ToString();
+            }
+        }
+
+        private static Text SplitToText(string input)
+        {
+            var result = new Text();
+            var sentences = input.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(var sent in sentences)
+            {
+                var words = sent.Split(new char[] { '.', ',', ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                result.Sentences.Add(new Sentence(words));
+            }
+
+            return result;
+        }
+    }
+}
