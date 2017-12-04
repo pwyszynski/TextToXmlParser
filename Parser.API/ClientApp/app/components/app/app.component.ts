e713faa5192cbf20ddb223771ce0d4ab980c054f
pwyszynski <wyszynski.p@windowslive.com>
2017-12-04 10:40:53 +0100

    Integrated all missing files

--- Parser.API/ClientApp/app/components/app/app.component.ts
+++ Parser.API/ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { XMLParserService } from '../../services/xmlparser.service';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'],
+	providers: [XMLParserService]
 })
 export class AppComponent {
 }
