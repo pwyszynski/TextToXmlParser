c16bd2fd5d08e77ba9fecad87d4a1528011b0232
pwyszynski <wyszynski.p@windowslive.com>
2017-12-04 19:37:30 +0100

    Removed unused components, cleaned up code, minor tweaks to splitter.

--- Parser.API/Services/XMLParserService/Models/Sentence.cs
+++ Parser.API/Services/XMLParserService/Models/Sentence.cs
@@ -6,12 +6,22 @@ using System.Xml.Serialization;
 
 namespace Parser.Models
 {
+	/// <summary>
+	/// Class representing a sentence.
+	/// </summary>
     public class Sentence
     {
+		/// <summary>
+		/// Default parameterless constructor for serialization.
+		/// </summary>
         public Sentence()
         {
-
         }
+
+		/// <summary>
+		/// Constructor that takes an array of words (strings) as parameter, sorts them and adds them to the internal Words list <see cref="Words"/>.
+		/// </summary>
+		/// <param name="words">Array of unsorted words (strings).</param>
         public Sentence(string[] words)
         {
             var sortedWords = words.ToList();
@@ -22,6 +32,9 @@ namespace Parser.Models
             }
         }
 
+		/// <summary>
+		/// List of words contained in the sentence.
+		/// </summary>
         [XmlElement("word")]
         public List<string> Words {get; set;} = new List<string>();
     }
