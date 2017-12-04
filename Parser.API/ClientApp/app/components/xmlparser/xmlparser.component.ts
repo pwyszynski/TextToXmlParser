9be16862cfac006964f899a6725e88a5935528e6
pwyszynski <wyszynski.p@windowslive.com>
2017-12-04 10:36:15 +0100

    Added client side service for Angular and component

---
+++ Parser.API/ClientApp/app/components/xmlparser/xmlparser.component.ts
@@ -0,0 +1,17 @@
+﻿import { XMLParserService } from "../../services/xmlparser.service";
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'xmlparser',
+	templateUrl: './xmlparser.component.html'
+})
+export class XmlParserComponent {
+	result: string;
+	text: string;
+
+	constructor(private xmlParser: XMLParserService) { }
+
+	getXmlFromText() {
+		this.xmlParser.getResult(this.text).subscribe(response => this.result = response.text(), error => console.log('Error parsing: ' + error))
+	}
+}
